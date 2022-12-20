ce731a329a83cf09d6a06d30d872b038d52b75d5
PMagoga <pmagoga@gmail.com>
2022-12-20 16:48:01 -0300

    Inserindo comentários no código

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 
     private static void Main(string[] args)
     {
+        //solicita ao usuário que escolha entre as 4 operações para executar
         Console.WriteLine("Qual operação deseja executar? ");
         Console.WriteLine("Digite (1) para Adição: ");
         Console.WriteLine("Digite (2) para Subtração: ");
@@ -15,12 +16,13 @@
         {
             case 1:
                 {
+                    //solicita ao usuário o primeiro número para efetuar a operação
                     Console.WriteLine("Digite o primeiro número: ");
                     int numero1 = int.Parse(Console.ReadLine());
-
+                    //solicita ao usuário o segundo número para efetuar a operação
                     Console.WriteLine("Digite o segundo número: ");
                     int numero2 = int.Parse(Console.ReadLine());
-
+                    //devolve o resultado da operação
                     Calculadora.Models.Calculadora.Somar(numero1, numero2);
                     break;
                 }
@@ -31,7 +33,7 @@
 
                     Console.WriteLine("Digite o segundo número: ");
                     int numero2 = int.Parse(Console.ReadLine());
-
+                    //devolve o resultado da operação
                     Calculadora.Models.Calculadora.Subtrair(numero1, numero2);
                     break;
                 }
@@ -42,7 +44,7 @@
 
                     Console.WriteLine("Digite o segundo número: ");
                     int numero2 = int.Parse(Console.ReadLine());
-
+                    //devolve o resultado da operação
                     Calculadora.Models.Calculadora.Multiplica(numero1, numero2);
                     break;
                 }
@@ -53,12 +55,13 @@
 
                     Console.WriteLine("Digite o segundo número: ");
                     int numero2 = int.Parse(Console.ReadLine());
-
+                    //devolve o resultado da operação
                     Calculadora.Models.Calculadora.Dividir(numero1, numero2);
                     break;
                 }
             default:
                 {
+                    //caso o usuário na digite uma opção não válida (a saber: 1, 2, 3 ou 4)
                     Console.WriteLine("Digite uma opção Válida...");
                     break;
                 }
